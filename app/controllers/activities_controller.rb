7a240d303431c695168ce2afb75f922af849c495
RolandSzentesi <rampageroli@gmail.com>
2023-09-04 01:03:33 -0500

    done

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,28 @@
+class ActivitiesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    # GET /activities
+    def index
+        activities = Activity.all
+        render json: activities, only: [:id, :name, :difficulty]
+    end
+
+    # DELETE /activities/:id
+
+    def destroy
+        activity = Activity.find(params[:id])
+        activity.destroy
+        head :no_content
+    end
+
+    private 
+
+    def render_not_found_response
+        render json: { error: "Activity not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+      end
+end
